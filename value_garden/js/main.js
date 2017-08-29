b380ffe329329fe81563419ca5fb0a2da1cca59d
Aleksandr-Poklad <aleksandr.poklad.ap@gmail.com>
2017-08-29 20:33:01 +0300

    value_garden

---
+++ value_garden/js/main.js
@@ -0,0 +1,35 @@
+$(document).ready( function() {
+
+
+	$(".mainMnuButton").click(function() {
+		$(".nav-left ul").slideToggle();
+	})
+
+
+	//Каруселька
+	//Документация: http://owlgraphic.com/owlcarousel/
+	var owl = $(".carousel");
+	owl.owlCarousel({
+		items : 1,
+		singleItem:true,
+		mouseDrag: false,
+		touchDrag: false,
+		autoPlay: true
+
+	});
+	owl.on(".owl-wrapper", function (e) {
+		if (e.deltaY > 0) {
+			owl.trigger("owl.prev");
+		} else {
+			owl.trigger("owl.next");
+		}
+		e.preventDefault();
+	});
+	$(".next_button").click(function(){
+		owl.trigger("owl.next");
+	});
+	$(".prev_button").click(function(){
+		owl.trigger("owl.prev");
+	});
+    
+});
