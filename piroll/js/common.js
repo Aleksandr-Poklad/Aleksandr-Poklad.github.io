f4fb063817dfd673121e3cf5dbf8e08218df4bf6
Aleksandr-Poklad <aleksandr.poklad.ap@gmail.com>
2017-12-28 22:03:47 +0200

    piroll commit

--- piroll/js/common.js
+++ piroll/js/common.js
@@ -1,67 +1,64 @@
 $(document).ready(function(){
 //Плавный переход к нижнему блоку
-    $("#menu, #button_up, .contact_button").on("click","a", function (event) {
+$("#menu, #button_up, .contact_button").on("click","a", function (event) {
         //отменяем стандартную обработку нажатия по ссылке
         event.preventDefault();
-        //забираем идентификатор бока с атрибута href
-        var id  = $(this).attr('href'),
+        //забираем идентификатор блока с атрибута href
+        var href  = $(this).attr('href'),
         //узнаем высоту от начала страницы до блока на который ссылается якорь
-        top = $(id).offset().top;
+        top = $(href).offset().top;
         //анимируем переход на расстояние - top за 1500 мс
         $('body,html').animate({scrollTop: top}, 1500);
     });
 
 // Переключатель меню
 $("#menu").on("click", ".tab", function(){
-        $("#menu .tab").removeClass("active"); 
-        $(this).addClass("active"); 
-      });
-
-    // $(".menu_button").click(function() {
-    //     $(".nav ul").slideToggle();
-    // });
-    (function ($) {
-    	$(function () {
-    		$('.menu_button').on('click', function () {
-    			$(this).closest('.manu_wrap').toggleClass('menu-open');
-    		});
-    	});
-    })(jQuery);
+    $("#menu .tab").removeClass("active"); 
+    $(this).addClass("active"); 
+});
 
 
-    $(document).ready(function() {
-    	$('.line1').addClass("hidden").viewportChecker({
-    		classToAdd: 'line_1 animated line-1',
-    		offset: 100
-    	});
-    });
-    $(document).ready(function() {
-    	$('.line2').addClass("hidden").viewportChecker({
-    		classToAdd: 'line_2 animated line-2',
-    		offset: 100
-    	});
-    });
-    $(document).ready(function() {
-    	$('.line3').addClass("hidden").viewportChecker({
-    		classToAdd: 'line_3 animated line-3',
-    		offset: 100
-    	});
-    });
+(function ($) {
+ $(function () {
+  $('.menu_button').on('click', function () {
+   $(this).closest('.manu_wrap').toggleClass('menu-open');
+});
+});
+})(jQuery);
 
 
-    $('.slider').owlCarousel({
+$(document).ready(function() {
+ $('.line1').addClass("hidden").viewportChecker({
+  classToAdd: 'line_1 animated line-1',
+  offset: 100
+});
+});
+$(document).ready(function() {
+ $('.line2').addClass("hidden").viewportChecker({
+  classToAdd: 'line_2 animated line-2',
+  offset: 100
+});
+});
+$(document).ready(function() {
+ $('.line3').addClass("hidden").viewportChecker({
+  classToAdd: 'line_3 animated line-3',
+  offset: 100
+});
+});
+
+
+$(".slider").owlCarousel({
     loop:true,
     dots:true,
     nav:false,
     items:1,
     autoplay:true,
-   
+
 })
+$(".video_overlay").on("click", function () {
+    $(this).css("display", "none");
+});
 
+});
 
-  });
-
-document.querySelector(".video_overlay").onclick = function(){
-    	this.style.display = "none";
-    }
 
