0979192bccddada5f77ff3b00b64ee66d5a2f1a6
Aleksandr-Poklad <aleksandr.poklad.ap@gmail.com>
2017-12-28 22:13:25 +0200

    piroll commit

--- piroll/js/common.js
+++ piroll/js/common.js
@@ -1,63 +1,62 @@
 $(document).ready(function(){
-//Плавный переход к нижнему блоку
-$("#menu, #button_up, .contact_button").on("click","a", function (event) {
-        //отменяем стандартную обработку нажатия по ссылке
+//Smooth transition to the lower block
+    $("#menu, #button_up, .contact_button").on("click","a", function (event) {
+        //cancels standard click-through processing
         event.preventDefault();
-        //забираем идентификатор блока с атрибута href
+        //we take the block identifier from the href attribute
         var href  = $(this).attr('href'),
-        //узнаем высоту от начала страницы до блока на который ссылается якорь
+        //learn the height from the beginning of the page to the block that the anchor refers to
         top = $(href).offset().top;
-        //анимируем переход на расстояние - top за 1500 мс
+        //animate the transition to the distance - top for 1500 ms
         $('body,html').animate({scrollTop: top}, 1500);
     });
 
-// Переключатель меню
-$("#menu").on("click", ".tab", function(){
-    $("#menu .tab").removeClass("active"); 
-    $(this).addClass("active"); 
-});
+// menu switch
+    $("#menu").on("click", ".tab", function(){
+        $("#menu .tab").removeClass("active"); 
+        $(this).addClass("active"); 
+    });
 
 
-(function ($) {
- $(function () {
-  $('.menu_button').on('click', function () {
-   $(this).closest('.manu_wrap').toggleClass('menu-open');
-});
-});
-})(jQuery);
+    (function ($) {
+     $(function () {
+      $('.menu_button').on('click', function () {
+       $(this).closest('.manu_wrap').toggleClass('menu-open');
+    });
+    });
+    })(jQuery);
 
 
-$(document).ready(function() {
- $('.line1').addClass("hidden").viewportChecker({
-  classToAdd: 'line_1 animated line-1',
-  offset: 100
-});
-});
-$(document).ready(function() {
- $('.line2').addClass("hidden").viewportChecker({
-  classToAdd: 'line_2 animated line-2',
-  offset: 100
-});
-});
-$(document).ready(function() {
- $('.line3').addClass("hidden").viewportChecker({
-  classToAdd: 'line_3 animated line-3',
-  offset: 100
-});
-});
+    $(document).ready(function() {
+     $('.line1').addClass("hidden").viewportChecker({
+      classToAdd: 'line_1 animated line-1',
+      offset: 100
+    });
+    });
+    $(document).ready(function() {
+     $('.line2').addClass("hidden").viewportChecker({
+      classToAdd: 'line_2 animated line-2',
+      offset: 100
+    });
+    });
+    $(document).ready(function() {
+     $('.line3').addClass("hidden").viewportChecker({
+      classToAdd: 'line_3 animated line-3',
+      offset: 100
+    });
+    });
 
+    $(".slider").owlCarousel({
+        loop:true,
+        dots:true,
+        nav:false,
+        items:1,
+        autoplay:true,
 
-$(".slider").owlCarousel({
-    loop:true,
-    dots:true,
-    nav:false,
-    items:1,
-    autoplay:true,
-
-})
-$(".video_overlay").on("click", function () {
-    $(this).css("display", "none");
-});
+    })
+    $(".video_overlay").on("click", function () {
+        $(this).css("display", "none");
+    });
 
 });
 
