414e8d7717a8296fc831a2d8a8e73bd076f0212b
Aleksandr-Poklad <aleksandr.poklad.ap@gmail.com>
2018-01-11 01:17:06 +0200

    vh commit

---
+++ Velvet Hammer/js/common.js
@@ -0,0 +1,15 @@
+
+    // установливаем обработчик события resize
+    $( window ).resize(function() {
+        $( "content" ).css("width", $( window ).width() );
+      $( "content" ).css("height", $( window ).height() );
+  });
+
+// вызовем событие resize
+$(window).resize();
+
+
+
+
+
+
