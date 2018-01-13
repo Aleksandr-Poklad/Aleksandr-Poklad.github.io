078d74977b50a3a10a9b56e2760d88f5828fc38e
Aleksandr-Poklad <aleksandr.poklad.ap@gmail.com>
2018-01-13 22:00:15 +0200

    vh commit

--- Velvet_Hammer/js/common.js
+++ Velvet_Hammer/js/common.js
@@ -1,13 +1,20 @@
-
-    // установливаем обработчик события resize
-    $( window ).resize(function() {
-        $( "content" ).css("width", $( window ).width() );
-      $( "content" ).css("height", $( window ).height() );
-  });
+// установливаем обработчик события resize
+$( window ).resize(function() {
+	$( "content" ).css("width", $( window ).width() );
+	$( "content" ).css("height", $( window ).height() );
+});    
 // вызовем событие resize
 $(window).resize();
 
+$(document).ready(function () {
 
+$("#follow").on("click", function () {
+		$(".hidden_follow").toggle("display");
+	});
+
+
+
+});
 
 
 
