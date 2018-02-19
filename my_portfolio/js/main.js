8d27e54b0c4da544e86d23ab37e2e0b3c63e53b7
DarkStyleAP <darkstyle.ap@gmail.com>
2018-02-20 00:23:25 +0200

    my_portfolio commit

--- my_portfolio/js/main.js
+++ my_portfolio/js/main.js
@@ -1,38 +1,73 @@
-//Плавный переход к нижнему блоку
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 1500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPosition = $(document).scrollTop();
+    $('nav a').each(function () {
+        var currentLink = $(this);
+        var refElement = $(currentLink.attr("href"));
+        if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+            $('nav ul li a').removeClass("active");
+            currentLink.addClass("active");
+        }
+        else{
+            currentLink.removeClass("active");
+        }
+    });
+}
+
+
+
+$(window).scroll(function () {
+    parallax();
+})
+function parallax() {
+    var wScroll = $(window).scrollTop();
+    $(".parallax_bg").css("background-position", "center "+(wScroll*0.75)+"px");
+    $(".parallax_box").css("top", -130+(wScroll*0.17)+"px");
+}
+
+
 $(document).ready(function(){
 
 
-        /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-    particlesJS.load('particles-js', 'libs/particles/config.json', function() {
-      console.log('callback - particles.js config loaded');
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+  //   particlesJS.load('particles-js', 'libs/particles/config.json', function() {
+  //     console.log('callback - particles.js config loaded');
+  // });
+
+
+(function ($) {
+    $(function () {
+        $(".menu_button").on("click", function () {
+            $("#menu").slideToggle();
+            $(this).closest(".manu_wrap").toggleClass("menu-open");   
+        });
     });
-
-
-    $("#menu").on("click","a", function (event) {
-        //отменяем стандартную обработку нажатия по ссылке
-        event.preventDefault();
-        //забираем идентификатор бока с атрибута href
-        var id  = $(this).attr('href'),
-        //узнаем высоту от начала страницы до блока на который ссылается якорь
-        top = $(id).offset().top;
-        //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({scrollTop: top}, 1500);
-    });
-
-// Переключатель меню
-    $("#menu").on("click", ".tab", function(){
-        $("#menu .tab").removeClass("active"); //удаляем класс во всех вкладках
-        $(this).addClass("active"); //добавляем класс текущей (нажатой)
-    });
-
-    $(".menu_button").click(function() {
-        $(".nav ul").slideToggle();
-    });
-
-
-
-
-
-
+})(jQuery);
 
 });
+
+
+
